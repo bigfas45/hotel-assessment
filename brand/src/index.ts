57b5a3de80d3fb7e811f4da80e287b1e4ad1cbd4
bigfas45 <ayodimejifasina@gmail.com>
2022-11-05 05:21:12 +0100

    update

--- brand/src/index.ts
+++ brand/src/index.ts
@@ -45,6 +45,7 @@ const start = async () => {
     process.on('SIGTERM', () => natsWrapper.client.close());
 
 
+    
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
